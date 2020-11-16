c6c86f41081e3a1732ee87618383c90e415f4815
jaroon09et <68173312+jaroon09et@users.noreply.github.com>
2020-11-16 15:34:03 +0700

    web.json

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ var http = require('http');
 
 // Configure HTTP server to respond with Hello World to all requests
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(3000, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
 
